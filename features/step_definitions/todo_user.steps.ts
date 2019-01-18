98b6ad65efb4c713e27998b076028d58a781b69c
Paolo Carrasco <paolo.carrasco@gmail.com>
2019-01-18 20:23:39 +0100

    Global stage for actor is set

--- features/step_definitions/todo_user.steps.ts
+++ features/step_definitions/todo_user.steps.ts
@@ -1,6 +1,3 @@
-import {protractor} from 'protractor';
-import {serenity} from 'serenity-js';
-import {Actor, BrowseTheWeb} from 'serenity-js/protractor';
 import {expect} from '../../spec/expect';
 import {listOf} from '../../spec/text';
 import {TodoList} from '../screenplay/targets/todo-list';
@@ -9,23 +6,20 @@ import {Start} from '../screenplay/tasks/start';
 
 export = function todoUserSteps() {
 
-    this.setDefaultTimeout(30 * 1000);
-
-    const stage = serenity.callToStageFor({
-        actor: name => Actor.named(name).whoCan(BrowseTheWeb.using(protractor.browser)),
+    this.Given(/^that (.+) has a TODO list containing (.*)$/, function(name: string, items: string) {
+        return this.stage.theActorCalled(name).attemptsTo(
+            Start.withATodoListContaining(listOf(items)),
+        );
     });
 
-    this.Given(/^that (.+) has a TODO list containing (.*)$/, (name: string, items: string) =>
-        stage.theActorCalled(name).attemptsTo(
-            Start.withATodoListContaining(listOf(items)),
-        ));
-
-    this.When(/^s?he adds (.*?) to (?:his|her) list$/, (itemName: string) =>
-        stage.theActorInTheSpotlight().attemptsTo(
+    this.When(/^s?he adds (.*?) to (?:his|her) list$/, function(itemName: string) {
+        return this.stage.theActorInTheSpotlight().attemptsTo(
             AddTodoItem.called(itemName),
-        ));
+        );
+    });
 
-    this.Then(/^.* todo list should contain (.*?)$/, (items: string) =>
-        expect(stage.theActorInTheSpotlight().toSee(TodoList.Items_Displayed))
-            .eventually.deep.equal(listOf(items)));
+    this.Then(/^.* todo list should contain (.*?)$/, function(items: string) {
+        return expect(this.stage.theActorInTheSpotlight().toSee(TodoList.Items_Displayed))
+            .eventually.deep.equal(listOf(items));
+    });
 };
---
+++ features/support/world.ts
@@ -0,0 +1,20 @@
+import { serenity } from 'serenity-js';
+
+import { protractor } from 'protractor';
+import { Actor, BrowseTheWeb, Cast } from 'serenity-js/protractor';
+
+export = function() {
+
+    this.setDefaultTimeout(60 * 1000);
+
+    this.World = function() {
+        // Any properties assigned to `this` will be available to step definitions:
+        this.stage = serenity.callToStageFor(new Seereners());
+    };
+};
+
+class Seereners implements Cast {
+    actor(name: string): Actor {
+        return Actor.named(name).whoCan(BrowseTheWeb.using(protractor.browser));
+    }
+}
